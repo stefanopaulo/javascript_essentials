454409841cb56d89697a7416322b7c4ddb1e1c0b
stefanopaulo <stefanopaulo47@gmail.com>
2020-04-01 18:25:27 -0300

    iniciando com o DOM do JS

--- modulo-c/exe-005/script.js
+++ modulo-c/exe-005/script.js
@@ -1,1 +1,18 @@
-console.log("olá, mundo!")
+var p1 = document.getElementsByTagName('p')[1]
+
+document.write(p1.innerHTML)
+
+p1.style.color = 'blue'
+
+var title = document.getElementById('titulo')
+
+title.innerHTML = "Seja Bem Vindo!"
+
+title.style.background = 'tomato'
+title.style.color = '#fff'
+title.style.width = '300px'
+title.style.height = '300px'
+
+var msg = document.querySelector('div.msg')
+
+msg.innerHTML = 'Selecionado por Seletor'
