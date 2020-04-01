1fbf34a2f0f6feafaf675b4d90b99b4d10f2d5f9
stefanopaulo <stefanopaulo47@gmail.com>
2020-04-01 18:57:11 -0300

    eventos DOM

---
+++ modulo-c/exe-006/script.js
@@ -0,0 +1,19 @@
+var a = document.getElementById('area')
+
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
+function clicar() {
+    a.innerText = 'Clicou!'
+    a.style.background = 'red'
+}
+
+function entrar() {
+    a.innerText = 'Entrou!'
+}
+
+function sair() {
+    a.innerText = 'Saiu!'
+    a.style.background = '#2a8b2a'
+}
