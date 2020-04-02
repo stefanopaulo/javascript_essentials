9d9528aad36c63c87f75dc3f2e53f956eb80c471
stefanopaulo <stefanopaulo47@gmail.com>
2020-04-02 20:41:47 -0300

    exercicio verificador de idade

---
+++ exercicios/exe-016/script.js
@@ -0,0 +1,56 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'homemcrianca.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'homemjovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'homemadulto.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'homemvelho.png')
+            }
+            
+        } else if(fsex[1].checked) {
+            genero = 'Mulher '
+            
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'mulhercrianca.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'mulherjovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'mulheradulta.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'mulhervelha.png')
+            }
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
