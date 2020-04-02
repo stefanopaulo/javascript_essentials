96beaf314b02a4dca627813bfbd972d78bbb4bb6
stefanopaulo <stefanopaulo47@gmail.com>
2020-04-02 18:20:58 -0300

    condicionais aninhadas e switch..case

---
+++ modulo-d/exe-014/script.js
@@ -0,0 +1,46 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+/*
+    0 - DOMINGO
+    1 - SEGUNDA
+    2 - TERÇA
+    3 - QUARTA
+    4 - QUINTA
+    5 - SEXTA
+    6 - SABÁDO
+*/
+
+switch(diaSem) {
+    case 0: 
+        console.log('Domingo') 
+    break
+
+    case 1: 
+        console.log('Segunda-Feira') 
+    break
+
+    case 2: 
+        console.log('Terça-Feira') 
+    break
+
+    case 3: 
+        console.log('Quarta-Feira') 
+    break
+
+    case 4: 
+        console.log('Quinta-Feira') 
+    break
+
+    case 5: 
+        console.log('Sexta-Feira') 
+    break
+
+    case 6: 
+        console.log('Sabádo') 
+    break
+
+    default:
+        console.log('Dia Inválido')
+    break
+}
